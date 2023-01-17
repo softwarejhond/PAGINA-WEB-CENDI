6f8cc9358c12e914104280483ccdc639656731c1
jhonD94 <56081015+jhonD94@users.noreply.github.com>
2023-01-16 19:40:17 -0500

    add new logos the company

--- index.php
+++ index.php
@@ -570,69 +570,90 @@
             </div>
             <div class="row" style="text-align: center;">
                 <div class="col-md-6 col-lg-3">
-                    <div class="member"><img src="img/icons/celac-adm.png" alt="celac">
+                    <div class="member"><img src="img/icons/celac-adm.png" alt="celac" width="150">
 
                     </div>
                 </div>
                 <div class="col-md-6 col-lg-3">
                     <div class="member">
-                        <div class="member"><img src="img/icons/logo-sena.png" alt="sena">
-
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6 col-lg-3">
-                    <div class="member">
-                        <div class="member"><img src="img/icons/Min.png" alt="min">
+                        <div class="member"><img src="img/icons/logo-sena.png" alt="sena" width="150">
 
                         </div>
                     </div>
                 </div>
                 <div class="col-md-6 col-lg-3">
                     <div class="member">
-                        <div class="member"><img src="img/icons/Secedu.png" alt="secedu">
+                        <div class="member"><img src="img/icons/Min.png" alt="min" width="150">
 
                         </div>
                     </div>
                 </div>
                 <div class="col-md-6 col-lg-3">
                     <div class="member">
-                        <div class="member"><img src="img/icons/Q10.png" alt="q10">
+                        <div class="member"><img src="img/icons/Secedu.png" alt="secedu" width="150">
 
                         </div>
                     </div>
                 </div>
                 <div class="col-md-6 col-lg-3">
                     <div class="member">
-                        <div class="member"><img src="img/icons/Mega.png" alt="mega" width="100%">
+                        <div class="member"><img src="img/icons/Q10.png" alt="q10" width="150">
 
                         </div>
                     </div>
                 </div>
                 <div class="col-md-6 col-lg-3">
                     <div class="member">
-                        <div class="member"><img src="img/icons/mejores.png" alt="mejores">
+                        <div class="member"><img src="img/icons/Mega.png" alt="mega" width="100%" width="150">
 
                         </div>
                     </div>
                 </div>
                 <div class="col-md-6 col-lg-3">
                     <div class="member">
-                        <div class="member"><img src="img/icons/lineaVital.png" alt="Droguería Linea Vital">
+                        <div class="member"><img src="img/icons/mejores.png" alt="mejores" width="150">
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3">
+                    <div class="member">
+                        <div class="member"><img src="img/icons/lineaVital.png" alt="Droguería Linea Vital" width="150">
 
                         </div>
                     </div>
                 </div>
 				<div class="col-md-6 col-lg-3">
                     <div class="member">
-                        <div class="member"><img src="img/icons/domina.png" alt="Domina">
+                        <div class="member"><img src="img/icons/domina.png" alt="Domina" width="150">
 
                         </div>
                     </div>
                 </div>
 				<div class="col-md-6 col-lg-3">
                     <div class="member">
-                        <div class="member"><img src="img/icons/clinica.png" alt="Fundación Clinica del Norte">
+                        <div class="member"><img src="img/icons/clinica.png" alt="Fundación Clinica del Norte" width="150">
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3">
+                    <div class="member">
+                        <div class="member"><img src="img/icons/dropopular.png" alt="DROPOPULAR" width="200">
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3">
+                    <div class="member">
+                        <div class="member"><img src="img/icons/coe.png" alt="Coe" width="150">
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3">
+                    <div class="member">
+                        <div class="member"><img src="img/icons/belleza.png" alt="Has Belleza" width="150px">
 
                         </div>
                     </div>
