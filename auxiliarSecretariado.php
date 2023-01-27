2747a72ece0d12101ba85d11631ad45dfb75d8b3
jhonD94 <56081015+jhonD94@users.noreply.github.com>
2023-01-26 20:45:19 -0500

    update information

--- auxiliarSecretariado.php
+++ auxiliarSecretariado.php
@@ -80,7 +80,9 @@
                                         </div>
                                         <div class="tab-pane fade" id="plan" role="tabpanel" aria-labelledby="list-profile-list"style="text-align: justify;">
                                             <h3>PLAN DE ESTUDIO:</h3>
-                                            El programa Técnico Laboral como Auxiliar Administrativo, cuenta con un total de 1.200 horas, distribuidas en tres niveles de formación: 50% teórico (600 horas) y 50% prácticas (600 horas). Las horas prácticas serán de cumplimiento en las instituciones en que se tengan convenio.
+                                            El programa técnico laboral como auxiliar en secretariado gerencial, cuenta con un total de 1.200 horas, 
+                                            distribuidas en tres niveles de formación: 50% teórico (600 horas) y 50% prácticas (600 horas). Las 
+                                            horas prácticas serán de cumplimiento en las instituciones en que se tenga convenio.
                                         </div>
                                         <div class="tab-pane fade" id="duracion" role="tabpanel" aria-labelledby="list-messages-list"style="text-align: justify;">
                                             
