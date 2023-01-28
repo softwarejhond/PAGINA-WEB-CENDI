89ded19ec963923d0f2b97d7fa4c8d2eb10e4e3b
jhonD94 <56081015+jhonD94@users.noreply.github.com>
2023-01-27 20:08:55 -0500

    new logos

---
+++ convenios.php
@@ -0,0 +1,113 @@
+<section class="team-section spad">
+        <div class="container">
+            <div class="section-title text-center">
+                <h3>NUESTROS CONVENIOS</h3>
+
+            </div>
+            <div class="row" style="text-align: center;">
+                <div class="col-md-6 col-lg-3">
+                    <div class="member"><img src="img/icons/celac-adm.png" alt="celac" width="150">
+
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3">
+                    <div class="member">
+                        <div class="member"><img src="img/icons/logo-sena.png" alt="sena" width="150">
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3">
+                    <div class="member">
+                        <div class="member"><img src="img/icons/Min.png" alt="min" width="150">
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3">
+                    <div class="member">
+                        <div class="member"><img src="img/icons/Secedu.png" alt="secedu" width="150">
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3">
+                    <div class="member">
+                        <div class="member"><img src="img/icons/Q10.png" alt="q10" width="150">
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3">
+                    <div class="member">
+                        <div class="member"><img src="img/icons/Mega.png" alt="mega" width="100%" width="150">
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3">
+                    <div class="member">
+                        <div class="member"><img src="img/icons/mejores.png" alt="mejores" width="150">
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3">
+                    <div class="member">
+                        <div class="member"><img src="img/icons/lineaVital.png" alt="Droguería Linea Vital" width="150">
+
+                        </div>
+                    </div>
+                </div>
+				<div class="col-md-6 col-lg-3">
+                    <div class="member">
+                        <div class="member"><img src="img/icons/domina.png" alt="Domina" width="150">
+
+                        </div>
+                    </div>
+                </div>
+				<div class="col-md-6 col-lg-3">
+                    <div class="member">
+                        <div class="member"><img src="img/icons/clinica.png" alt="Fundación Clinica del Norte" width="150">
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3">
+                    <div class="member">
+                        <div class="member"><img src="img/icons/dropopular.png" alt="DROPOPULAR" width="200">
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3">
+                    <div class="member">
+                        <div class="member"><img src="img/icons/coe.png" alt="Coe" width="150">
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3">
+                    <div class="member">
+                        <div class="member"><img src="img/icons/belleza.png" alt="Has Belleza" width="150px">
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3">
+                    <div class="member">
+                        <div class="member"><img src="img/icons/sanigancio.png" alt="Colegio San Ignacio" width="150px">
+
+                        </div>
+                    </div>
+                </div> <div class="col-md-6 col-lg-3">
+                    <div class="member">
+                        <div class="member"><img src="img/icons/fenalco.png" alt="Fenalco" width="150px">
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
