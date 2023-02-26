3783130fe6389eb79e1317eea9e6720b54dbb313
jhonD94 <56081015+jhonD94@users.noreply.github.com>
2023-02-26 16:20:32 -0500

    New item

---
+++ personal.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'head.php'; ?>
+
+<body>
+
+    <!-- Seccion de encabezado inicia-->
+    <?php include 'header.php';?>
+    <!-- Seccion de encabezado finaliza-->
+    <br><br>
+
+    <!-- Menu encabezado inicia -->
+    <?php include 'navbar.php';?>
+    <!-- Menu encabezado finaliza-->
+
+    <!-- Indetifacion de pagina inicia -->
+    <div class="site-breadcrumb">
+        <div class="container">
+            <a href="index.html"><i class="fa fa-home"></i> Inicio</a> <i class="fa fa-angle-right"></i>
+            <span>Institución</span><i class="fa fa-angle-right"></i>
+            <span>Nuestro personal CENDI</span>
+        </div>
+    </div>
+    <!-- Indetifacion de pagina finaliza-->
+
+
+    <!-- Nosotros seccion -->
+    <section class="about-section spad pt-0">
+        <div class="container">
+            <div class="section-title text-center">
+                <h3>CONOCE NUESTRO PERSONAL CENDI</h3>
+            </div>
+            <div class="row">
+                <div class="col-lg-3 about-text">
+                    <div class="card" style="width: 100%">
+                        <img src="./img/member/20.png" class="card-img-top" alt="...">
+                    </div>
+                </div>
+                <div class="col-lg-3 about-text">
+                    <div class="card" style="width: 100%">
+                        <img src="./img/member/4.png" class="card-img-top" alt="...">
+                    </div>
+                </div>
+                <div class="col-lg-3 about-text">
+                    <div class="card" style="width: 100%">
+                        <img src="./img/member/14.png" class="card-img-top" alt="...">
+                    </div>
+                </div>
+                <div class="col-lg-3 about-text">
+                    <div class="card" style="width: 100%">
+                        <img src="./img/member/3.png" class="card-img-top" alt="...">
+                    </div>
+                </div>
+                <div class="col-lg-3 about-text">
+                    <div class="card" style="width: 100%">
+                        <img src="./img/member/12.png" class="card-img-top" alt="...">
+                    </div>
+                </div>
+                <div class="col-lg-3 about-text">
+                    <div class="card" style="width: 100%">
+                        <img src="./img/member/5.png" class="card-img-top" alt="...">
+                    </div>
+                </div>
+                <div class="col-lg-3 about-text">
+                    <div class="card" style="width: 100%">
+                        <img src="./img/member/16.png" class="card-img-top" alt="...">
+                    </div>
+                </div>
+                <div class="col-lg-3 about-text">
+                    <div class="card" style="width: 100%">
+                        <img src="./img/member/13.png" class="card-img-top" alt="...">
+                    </div>
+                </div>
+                <div class="col-lg-3 about-text">
+                    <div class="card" style="width: 100%">
+                        <img src="./img/member/10.png" class="card-img-top" alt="...">
+                    </div>
+                </div>
+                <div class="col-lg-3 about-text">
+                    <div class="card" style="width: 100%">
+                        <img src="./img/member/6.png" class="card-img-top" alt="...">
+                    </div>
+                </div>
+                <div class="col-lg-3 about-text">
+                    <div class="card" style="width: 100%">
+                        <img src="./img/member/2.png" class="card-img-top" alt="...">
+                    </div>
+                </div>
+                <div class="col-lg-3 about-text">
+                    <div class="card" style="width: 100%">
+                        <img src="./img/member/1.png" class="card-img-top" alt="...">
+                    </div>
+                </div>
+                <div class="col-lg-3 about-text">
+                    <div class="card" style="width: 100%">
+                        <img src="./img/member/7.png" class="card-img-top" alt="...">
+                    </div>
+                </div>
+                <div class="col-lg-3 about-text">
+                    <div class="card" style="width: 100%">
+                        <img src="./img/member/8.png" class="card-img-top" alt="...">
+                    </div>
+                </div>
+                <div class="col-lg-3 about-text">
+                    <div class="card" style="width: 100%">
+                        <img src="./img/member/11.png" class="card-img-top" alt="...">
+                    </div>
+                </div>
+                <div class="col-lg-3 about-text">
+                    <div class="card" style="width: 100%">
+                        <img src="./img/member/9.png" class="card-img-top" alt="...">
+                    </div>
+                </div>
+                <div class="col-lg-3 about-text">
+                    <div class="card" style="width: 100%">
+                        <img src="./img/member/19.png" class="card-img-top" alt="...">
+                    </div>
+                </div>
+                <div class="col-lg-3 about-text">
+                    <div class="card" style="width: 100%">
+                        <img src="./img/member/15.png" class="card-img-top" alt="...">
+                    </div>
+                </div>
+                <div class="col-lg-3 about-text">
+                    <div class="card" style="width: 100%">
+                        <img src="./img/member/18.png" class="card-img-top" alt="...">
+                    </div>
+                </div>
+                <div class="col-lg-3 about-text">
+                    <div class="card" style="width: 100%">
+                        <img src="./img/member/17.png" class="card-img-top" alt="...">
+                    </div>
+                </div>
+               
+             
+            </div>
+        </div>
+    </section>
+    <!-- Nosotros seccion termina-->
+    <!-- Footer section -->
+    <?php include 'footer.php'; ?>
+    <!-- Footer section end-->
+
+
+    <!--====== Javascripts & Jquery ======-->
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.countdown.js"></script>
+    <script src="js/masonry.pkgd.min.js"></script>
+    <script src="js/magnific-popup.min.js"></script>
+    <script src="js/main.js"></script>
+    <!--====== Boostrap ======-->
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+    </script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
